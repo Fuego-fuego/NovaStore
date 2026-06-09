e6a2001eae59a3140c46153c5cc41982405947d8
Nathaniel <nathanielmujesia@gmail.com>
2026-06-09 17:40:13 +0300

    feat: establish project folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import {router } from './app/router';
+import { RouterProvider } from 'react-router-dom';
+
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<RouterProvider router={router}/>
 )
