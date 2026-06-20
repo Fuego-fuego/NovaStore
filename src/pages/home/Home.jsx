d3a8706a747c0b2060a0dbf6f3dca2f770654da5
Nathaniel <nathanielmujesia@gmail.com>
2026-06-20 15:41:23 +0300

    feat: create application footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Input from "../../components/ui/Input/Input"
+
 
 const Home = () => {
 
@@ -6,11 +6,7 @@ const Home = () => {
         <>
         <h1>NovaStore Home</h1>
 
-        <div className="container-custom grid gap-2">
-            <Input type="text" label="name" placeholder="John Doe"/>
-            <Input type="email" label="email" placeholder="example@mail.com"/>
-            <Input type="tel" label="telephone Number" placeholder="0700000000" />
-        </div>
+
         </>
     )
 }
